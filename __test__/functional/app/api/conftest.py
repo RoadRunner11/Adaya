0101b1e6adce807026ccb72d302b3775110464bc
Clement Adedeji <coadedeji@gmail.com>
2019-12-28 18:23:20 +0000

    functional tests

--- __test__/functional/app/api/conftest.py
+++ __test__/functional/app/api/conftest.py
@@ -19,17 +19,43 @@ def new_member():
     user.lastname = 'Analytics'
     return user
 
-@pytest.fixture(scope='module')
-def new_product():
-    product = Product(name='name')
-    product.price = 10
-    return product
+# @pytest.fixture(scope='module')
+# def new_product():
+#     product = Product(name='name')
+#     product.price = 10
+#     product.id = 2
+#     return product
 
 
 @pytest.fixture(scope='module')
-def member_order(new_product):
+def member_order():
+    first_product = Product(name='fname')
+    first_product.price = 10
+    first_product.id = 2
+
+    second_product = Product(name='sname')
+    second_product.price = 320
+    second_product.id = 3
+
+    third_product = Product(name='tname')
+    third_product.price = 420
+    third_product.id = 4
+
+    fourth_product = Product(name='ftname')
+    fourth_product.price = 520
+    fourth_product.id = 5
+
+    fifth_product = Product(name='ftname')
+    fifth_product.price = 620
+    fifth_product.id = 6
+
     order = Order()
     order.products = []
-    order.products.append(new_product)
-    order.products.append(new_product)
+
+    order.products.append(first_product)
+    order.products.append(second_product)
+    order.products.append(third_product)
+    order.products.append(fourth_product)
+    order.products.append(fifth_product)
+    
     return order
--- __test__/functional/app/api/v1/test_user_order.py
+++ __test__/functional/app/api/v1/test_user_order.py
@@ -1,17 +1,24 @@
 from app.models import Order
 from app.helpers.app_context import AppContext as AC
 
-def test_member_order(test_client, init_database, member_order):
+def test_member_order(test_client, init_database, member_order):     
      response = test_client.post(
-        '/orders', json={'product_ids': [member_order.products[0].id, member_order.products[1].id], 'user_id': member_order.user_id})
+       '/orders', json={'product_ids':[member_order.products[0].id, member_order.products[1].id, member_order.products[2].id,
+                        member_order.products[3].id, member_order.products[4].id], 'user_id': member_order.user_id})
+     
+     second_response = test_client.post(
+       '/orders', json={'product_ids':[member_order.products[0].id, member_order.products[1].id], 'user_id': member_order.user_id})
+    
 
      assert response.status_code == 200
- 
-#  orders = Order.get_items(user_id=1)
-#     assert len(orders) == 10
-#     new_order.calculate_cost()
-#     new_order.insert()
-#     orders = Order.get_items(sort_by='total_price',is_desc=True)
-#     assert orders[0].total_price == new_order.total_price
-#     orders = Order.get_items(sort_by='total_price',is_desc=False)
-#     assert orders[0].total_price != new_order.total_price
+     assert second_response.status_code == 200
+
+     # ensure no products are sent back if more than 4 in order
+     assert len(response.json['body']['products'] ) == 0
+
+     assert len(second_response.json['body']['products'] ) == 2
+     
+
+    
+   
+  
