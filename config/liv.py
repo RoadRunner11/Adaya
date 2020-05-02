310d7fbe640d8264623cef546f8aa2a7c3c2edc6
Clement Adedeji <coadedeji@gmail.com>
2020-05-02 20:57:23 +0100

    db testting

--- config/liv.py
+++ config/liv.py
@@ -5,7 +5,7 @@ import os
 SECRET_KEY = 'AT@u@5HgZ/g0zK)a<-1l1[%xihmk-|'
 # database setting
 #SQLALCHEMY_DATABASE_URI = os.environ.get('CLEARDB_DATABASE_URL')
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://b5bd39bb94a0b0:7ff81330@eu-cdbr-west-02.cleardb.net/heroku_92f04b025bf199c?charset=utf8mb4'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bd39bb94a0b0:7ff81330@eu-cdbr-west-02.cleardb.net/heroku_92f04b025bf199c?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_ACCESS_TOKEN_EXPIRES = datetime.timedelta(days=365)
 JWT_TOKEN_LOCATION = ['cookies']
